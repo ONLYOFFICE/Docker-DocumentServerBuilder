e211fba388e9a9ece221d89c0be443c696b113a2
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2016-08-08 19:28:33 +0300

    Changed the build sequence

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 BUILD_DIR=/var/lib/onlyoffice
 
-cd $BUILD_DIR/sdkjs && make
-cd $BUILD_DIR/core/Common/3dParty && make.sh
+cd $BUILD_DIR/core/Common/3dParty && ./make.sh
 cd $BUILD_DIR/core && make
+cd $BUILD_DIR/sdkjs && make
 cd $BUILD_DIR/server && make
