5bedcbda7b62ef284433d7ebabbad486a14c80b0
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2016-10-07 12:50:56 +0300

    Added building packages from docker

---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BUILD_DIR=/var/lib/onlyoffice
+
+cd $BUILD_DIR/document-server-integration/web/documentserver-example/nodejs && npm install
+cd $BUILD_DIR/document-server-package && make all
